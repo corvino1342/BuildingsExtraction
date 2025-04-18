dcd5780c9c88cd05da11e43f43c700e7348e9cd8
corvino <corvino1342@gmail.com>
2025-04-18 11:28:31 +0200

    Adding some comments, to be checked

--- unet_architecture.py
+++ unet_architecture.py
@@ -1,5 +1,9 @@
 from libraries import *
 
+
+# Class to define the double convolution for the UNet.
+# After each 2D convolution we havea a batch normalization and a ReLU activation function
+# The size for the convolution is 3x3 with 1 padding
 class DoubleConv(nn.Module):
 
     def __init__(self, in_channels, out_channels, mid_channels=None):
@@ -19,6 +23,7 @@ class DoubleConv(nn.Module):
             return self.double_conv(x)
 
 
+# Downsampling using a pooling of 2x2
 class Down(nn.Module):
 
     def __init__(self, in_channels, out_channels):
@@ -32,6 +37,8 @@ class Down(nn.Module):
     def forward(self, x):
         return self.maxpool_conv(x)
 
+
+# Upsampling using a pooling of 2x2
 class Up(nn.Module):
 
     def __init__(self, in_channels, out_channels, bilinear=True):
@@ -53,16 +60,17 @@ class Up(nn.Module):
 
         x1 = F.pad(x1, [diffX//2, diffX - diffX//2,
                         diffY//2, diffY - diffY//2])
-''' 
+        ''' 
         For padding issues:
         https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-'''
-
+        '''
 
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
+
+# The output of the Convolutional Network
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(OutConv, self).__init__()
