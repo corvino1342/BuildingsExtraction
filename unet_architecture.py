e143d07ebdd176dba3d76143610f8c42c7e31a32
corvino <corvino1342@gmail.com>
2025-04-18 11:44:50 +0200

    Each step is explained

--- unet_architecture.py
+++ unet_architecture.py
@@ -2,8 +2,8 @@ from libraries import *
 
 
 # Class to define the double convolution for the UNet.
-# After each 2D convolution we havea a batch normalization and a ReLU activation function
-# The size for the convolution is 3x3 with 1 padding
+# After each 2D convolution we have a batch normalization and a ReLU activation function
+# The size for the convolution is 3x3 with 1 padding to get the same dimension
 class DoubleConv(nn.Module):
 
     def __init__(self, in_channels, out_channels, mid_channels=None):
@@ -23,7 +23,10 @@ class DoubleConv(nn.Module):
             return self.double_conv(x)
 
 
-# Downsampling using a pooling of 2x2
+# Downsampling using a pooling of 2x2.
+# It reduces the spatial dimensions using max pooling, then applies the DoubleConv defined above
+# This is the step of reducing the resolution of the image progressively with this encoding step
+# while increasing feature depth
 class Down(nn.Module):
 
     def __init__(self, in_channels, out_channels):
@@ -39,20 +42,30 @@ class Down(nn.Module):
 
 
 # Upsampling using a pooling of 2x2
+# This step increases the spatial dimensions using either bilinear interpolation or transposed convolution
+# After that, there is a feature map from the downsampling path.
+# This is the decoding step, divided into upsample -> combine -> refine
 class Up(nn.Module):
 
     def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
 
-
+        # These are the 2 upsampling methods, based on the flag bilinear. DoubleConv is used to refine features
+        # This does a smooth interpolation
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
             self.conv = DoubleConv(in_channels, out_channels, in_channels//2)
+        # This uses a learnable upsampling layer
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
 
+
+    # x1 is the lower-resolution feature map from the decoder
+    # x2 is the matching feature map from the encoder (skip connection)
     def forward(self, x1, x2):
+
+        # This is useful to align the dimensions of x1 and x2
         x1 = self.up(x1)
 
         diffY = x2.size()[2] - x1.size()[2]
@@ -66,11 +79,14 @@ class Up(nn.Module):
         https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         '''
 
+        # This is used to concatenate along the channel dimension x1 and x2. The .conv is used to fuse them
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
 
 # The output of the Convolutional Network
+# The final layer that converts the deep feature map into the desired number of output channels
+# Basically, it is a classifier
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(OutConv, self).__init__()
