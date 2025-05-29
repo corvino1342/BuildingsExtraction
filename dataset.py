92b329041199f99e866f031a578fb90eef74a75d
corvino <corvino1342@gmail.com>
2025-05-29 13:05:02 +0200

    Changed the condition for selecting the images in the directory

--- dataset.py
+++ dataset.py
@@ -7,7 +7,7 @@ class MyDataset(Dataset):
         self.image_dir = image_dir                              # here I can choose both the georef or the images directory
         self.mask_dir = mask_dir                                # these are the labels of the data
         self.transform = transform                              # the transformation for the data augmentation, for example
-        self.image_filenames = sorted(os.listdir(image_dir))    # the file names in the directory
+        self.image_filenames = sorted(f for f in os.listdir(image_dir) if f.startswith("tile"))    # the file names in the directory
 
     def __len__(self):
         return len(self.image_filenames)
@@ -15,18 +15,17 @@ class MyDataset(Dataset):
     def __getitem__(self, idx):
 
         # these return strings of the path and the name of each image
-        if 'tile' in self.image_filenames[idx]:
-            image_path = os.path.join(self.image_dir, self.image_filenames[idx])
-            mask_path = os.path.join(self.mask_dir, os.path.splitext(self.image_filenames[idx])[0] + '.png')  # assume same name between images and masks
+        image_path = os.path.join(self.image_dir, self.image_filenames[idx])
+        mask_path = os.path.join(self.mask_dir, os.path.splitext(self.image_filenames[idx])[0] + '.png')  # assume same name between images and masks
 
-            # we need a 3 channels input data, that's why we convert the images into RGB pictures
-            image = Image.open(image_path).convert("RGB")   # converting the images in RGB pictures
-            mask = Image.open(mask_path).convert("L")       # grayscale mask
+        # we need a 3 channels input data, that's why we convert the images into RGB pictures
+        image = Image.open(image_path).convert("RGB")   # converting the images in RGB pictures
+        mask = Image.open(mask_path).convert("L")       # grayscale mask
 
 
-            # if any transformation is done, here is applied on both images and masks
-            if self.transform:
-                image = self.transform(image)
-                mask = self.transform(mask)
+        # if any transformation is done, here is applied on both images and masks
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
 
         return image, mask
