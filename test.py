8c79e98086dd36973f5285b0f9749f94c4fbfa7d
corvino <corvino1342@gmail.com>
2025-05-29 13:06:47 +0200

    Some small changes

--- test.py
+++ test.py
@@ -4,21 +4,28 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from unet import UNet
 
-# === Configurazione ===
-model_path = "trained_models/unet_1.pth"
-image_path = "dataset/images/tile40.png"  # Sostituisci con il tuo path
+# === Initial Configuration ===
 
-# === Carica il modello ===
+tile_number = 20
+
+model_path = "trained_models/unet_1.pth"
+
+image_path = f"dataset/test/images/tile{tile_number}.png"  # Sostituisci con il tuo path
+mask_path = f"dataset/test/masks/tile{tile_number}.png"
+
+
+# === Load the model ===
 model = UNet(n_channels=3, n_classes=1)
 model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
 model.eval()
 
-# === Prepara l'immagine ===
+# === Prepare the image ===
 transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
 img = Image.open(image_path).convert("RGB")
+mask = Image.open(mask_path).convert("L")
 input_tensor = transform(img).unsqueeze(0)  # Aggiunge dimensione batch → [1, 3, H, W]
 
 # === Inference ===
@@ -27,14 +34,19 @@ with torch.no_grad():
     prediction = torch.sigmoid(output)
     binary_mask = (prediction > 0.5).float()
 
-# === Visualizza ===
+# === Visualization ===
 plt.figure(figsize=(10, 4))
-plt.subplot(1, 2, 1)
+plt.subplot(1, 3, 1)
 plt.title("Input Image")
 plt.imshow(img)
 plt.axis("off")
 
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 2)
+plt.title("Real Mask")
+plt.imshow(mask, cmap="gray")
+plt.axis("off")
+
+plt.subplot(1, 3, 3)
 plt.title("Predicted Mask")
 plt.imshow(binary_mask.squeeze().numpy(), cmap="gray")
 plt.axis("off")
