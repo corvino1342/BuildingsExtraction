f60051d793a31b4ebc7848926c54b95a377ba360
corvino <corvino1342@gmail.com>
2025-06-04 12:58:39 +0200

    details

--- dataset.py
+++ dataset.py
@@ -3,9 +3,10 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 class MyDataset(Dataset):
-    def __init__(self, image_dir, mask_dir, data_aug=None):
+    def __init__(self, image_dir, mask_dir, transform=None):
         self.image_dir = image_dir                              # here I can choose both the georef or the images directory
         self.mask_dir = mask_dir                                # these are the labels of the data
+        self.transform = transform                              # the transformation for the data augmentation, for example
         self.image_filenames = sorted(f for f in os.listdir(image_dir) if f.startswith("tile"))    # the file names in the directory
 
     def __len__(self):
@@ -20,5 +21,11 @@ class MyDataset(Dataset):
         # we need a 3 channels input data, that's why we convert the images into RGB pictures
         image = Image.open(image_path).convert("RGB")   # converting the images in RGB pictures
         mask = Image.open(mask_path).convert("L")       # grayscale mask
+
+
+        # This is mandatory to use the correct format of the files: tensors
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
 
         return image, mask
