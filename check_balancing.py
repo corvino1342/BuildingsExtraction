ea8251115c4ba236b5ce3afa196401d129529364
corvino <corvino1342@gmail.com>
2025-12-10 10:39:17 +0100

    Adding the histogram to get the distribution of the building fraction

--- check_balancing.py
+++ check_balancing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 dataset_name = 'AerialImageDataset'
 dataset_type = 'train'
 
-gt_path = f'/Users/corvino/PycharmProjects/BuildingsExtraction/datasets/{dataset_name}/{dataset_type}/gt'
+gt_path = f'/Users/corvino/PycharmProjects/BuildingsExtraction/datasets/{dataset_name}/tiles_128/{dataset_type}/gt'
 full_maps = sorted(os.path.splitext(f)[0] for f in os.listdir(gt_path) if
                    f.lower().endswith(('.tif', '.tiff', '.png', '.jpg')))
 
@@ -28,5 +28,18 @@ for image_name in full_maps:
     # Building fraction
     bfraction.append(num_building / num_pixels)
 
-plt.hist(bfraction)
-plt.show()
+mean_b = np.mean(bfraction)
+std_b = np.std(bfraction)
+
+plt.figure(figsize=(10, 6))
+plt.hist(bfraction, bins=50, edgecolor="black")
+plt.xlabel("Building fraction")
+plt.ylabel("Frequency")
+plt.title("Building Fraction Distribution")
+plt.axvline(mean_b, linestyle="--", linewidth=1.5, label=f"Mean = {mean_b:.3f}")
+plt.axvline(mean_b - std_b, linestyle=":", linewidth=1, label=f"Mean - STD = {mean_b - std_b:.3f}")
+plt.axvline(mean_b + std_b, linestyle=":", linewidth=1, label=f"Mean + STD = {mean_b + std_b:.3f}")
+plt.grid(True, alpha=0.3)
+plt.legend()
+
+plt.savefig("building_fraction_distribution.png")
