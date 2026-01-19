3a63c82f34bf966bfc6752ebaa211b619dee7395
corvino <corvino1342@gmail.com>
2026-01-19 12:04:10 +0100

    some changes

--- unet.py
+++ unet.py
@@ -14,17 +14,17 @@ class DoubleConv(nn.Module):
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels=in_channels, out_channels=mid_channels, kernel_size=3, padding=1),
-            nn.InstanceNorm2d(mid_channels, affine=True),
-            #nn.BatchNorm2d(mid_channels),
+            nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=mid_channels, out_channels=out_channels, kernel_size=3, padding=1),
-            nn.InstanceNorm2d(out_channels, affine=True),
-            #nn.BatchNorm2d(out_channels),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
         return self.double_conv(x)
+
+
 
 # Downsampling using a pooling of 2x2.
 # It reduces the spatial dimensions using max pooling, then applies the DoubleConv defined above
@@ -140,12 +140,13 @@ class UNet1(nn.Module):
 
 # UNet2
 
-# Input-> [Down1]->                                              [Up1]-> Output
-#                  [Down2]->                              [Up2]->
-#                           [Down3]->              [Up3]->
-#                                    [Bottleneck]->
+# Input-> [Down1]->                                                              [Up1]-> Output
+#                  [Down2]->                                              [Up2]->
+#                           [Down3]->                              [Up3]->
+#                                    [Down4]->              [Up4]->
+#                                             [Bottleneck]->
 
-# Simple UNet in order to train in human-size time
+# The actual UNet to train. The training time is a lot bigger wrt the other
 
 class UNet2(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True):
@@ -157,11 +158,13 @@ class UNet2(nn.Module):
         self.inc = DoubleConv(n_channels, 32)
         self.down1 = Down(32, 64)
         self.down2 = Down(64, 128)
+        self.down3 = Down(128, 256)
         factor = 2 if bilinear else 1
-        self.down3 = Down(128, 256 // factor)
-        self.up1 = Up(256, 128 // factor, bilinear)
-        self.up2 = Up(128, 64 // factor, bilinear)
-        self.up3 = Up(64, 32, bilinear)
+        self.down4 = Down(256, 512 // factor)
+        self.up1 = Up(512, 256 // factor, bilinear)
+        self.up2 = Up(256, 128 // factor, bilinear)
+        self.up3 = Up(128, 64 // factor, bilinear)
+        self.up4 = Up(64, 32, bilinear)
         self.outc = OutConv(32, n_classes)
 
     def forward(self, x):
@@ -169,8 +172,10 @@ class UNet2(nn.Module):
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
-        x = self.up1(x4, x3)
-        x = self.up2(x, x2)
-        x = self.up3(x, x1)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
         logits = self.outc(x)
         return logits
