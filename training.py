f783ef81d190e9ef82831b33fa900f6d83d87f7e
corvino <corvino1342@gmail.com>
2026-02-03 12:33:41 +0100

    Here there is the code of the jupyter notebook. In Avola I need to copy and paste this code in the notebook

--- training.py
+++ training.py
@@ -1,3 +1,4 @@
+# %%
 import os
 from tqdm import tqdm
 
@@ -11,33 +12,17 @@ import numpy as np
 
 import time
 import csv
-
-# Which model I have to choose?
-
-# UNet1
-
-# Input-> [Down1]->                                                              [Up1]-> Output
-#                  [Down2]->                                              [Up2]->
-#                           [Down3]->                              [Up3]->
-#                                    [Down4]->              [Up4]->
-#                                             [Bottleneck]->
+from unet import UNet, UNetL, UNetLL
 
 
-# UNet2
-
-# Input-> [Down1]->                                              [Up1]-> Output
-#                  [Down2]->                              [Up2]->
-#                           [Down3]->              [Up3]->
-#                                    [Bottleneck]->
-
-from unet import UNet1
-
+# %%
 def iou_score(preds, targets, threshold=0.5, eps=1e-6):
     preds = torch.sigmoid(preds)
     preds = (preds > threshold).float()
     intersection = (preds * targets).sum(dim=(1, 2, 3))
     union = preds.sum(dim=(1, 2, 3)) + targets.sum(dim=(1, 2, 3)) - intersection
     return ((intersection + eps) / (union + eps)).mean()
+
 
 def precision_score(preds, targets, threshold=0.5, eps=1e-6):
     preds = torch.sigmoid(preds)
@@ -46,6 +31,7 @@ def precision_score(preds, targets, threshold=0.5, eps=1e-6):
     predicted_positive = preds.sum()
     return (true_positive + eps) / (predicted_positive + eps)
 
+
 def recall_score(preds, targets, threshold=0.5, eps=1e-6):
     preds = torch.sigmoid(preds)
     preds = (preds > threshold).float()
@@ -53,33 +39,54 @@ def recall_score(preds, targets, threshold=0.5, eps=1e-6):
     actual_positive = targets.sum()
     return (true_positive + eps) / (actual_positive + eps)
 
+
+class DiceLoss(nn.Module):
+    def __init__(self, smooth=1.0):
+        super().__init__()
+        self.smooth = smooth
+
+    def forward(self, logits, targets):
+        probs = torch.sigmoid(logits)
+
+        probs = probs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (probs * targets).sum()
+        dice = (2. * intersection + self.smooth) / (
+                probs.sum() + targets.sum() + self.smooth
+        )
+
+        return 1 - dice
+
+
+# %%
 starting_time = time.time()
 
-
-model_dataset = 'AID'
+architecture_name = 'unetLL'
+model_dataset = 'WHUtiles'
+dataset_name = 'WHUBuildingDataset'
 tile_dimension = 256
 batch_size = 32
 weightedBCE = True
+diceLoss = True
 earlystop = False
 
 # Initial setup
 device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 # device = 'cpu'
 print(f"Device: {device}\n")
-memory_fraction = 0.3
+memory_fraction = 1.
 print(f"Used a fraction of {memory_fraction} GPU's memory")
 print(f"Weighted BCE:\t{weightedBCE}")
 
-training_dataset_portion = 0.5
-validation_dataset_portion = 0.3
+training_dataset_portion = 1
+validation_dataset_portion = 0.5
 num_epochs = 30
-learning_rate = 1e-4
-
+learning_rate = 0.001
 
 best_val_loss = float("inf")
 patience = 5
 early_stop_counter = 0
-
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -87,11 +94,19 @@ transform = transforms.Compose([
 
 dataset_path = '/mnt/nas151/sar/Footprint/datasets/'
 
-dataset_kind = 'AerialImageDataset'
+WHU_goal = 'tiles'
+
+WHU_train_image_dir = f"{dataset_name}/{WHU_goal}/train/images"
+WHU_train_mask_dir = f"{dataset_name}/{WHU_goal}/train/gt"
+
+WHU_val_image_dir = f"{dataset_name}/{WHU_goal}/val/images"
+WHU_val_mask_dir = f"{dataset_name}/{WHU_goal}/val/gt"
+
+
 # Dataset and DataLoader
-train_dataset_full = MyDataset(image_dir=dataset_path + dataset_kind + f'/tiles_{tile_dimension}/train/images',
-                          mask_dir=dataset_path + dataset_kind + f'/tiles_{tile_dimension}/train/gt',
-                          transform=transform)
+train_dataset_full = MyDataset(image_dir=WHU_train_image_dir,
+                               mask_dir=WHU_train_mask_dir,
+                               transform=transform)
 
 # In order to train with fewer images, this part will mix and select a portion (dataset_portion) of the entire training dataset.
 num_samples = len(train_dataset_full)
@@ -101,9 +116,9 @@ train_dataset = Subset(train_dataset_full, indices)
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-val_dataset_full = MyDataset(image_dir=dataset_path + dataset_kind + f'/tiles_{tile_dimension}/val/images',
-                          mask_dir=dataset_path + dataset_kind + f'/tiles_{tile_dimension}/val/gt',
-                          transform=transform)
+val_dataset_full = MyDataset(image_dir=WHU_val_image_dir,
+                             mask_dir=WHU_val_mask_dir,
+                             transform=transform)
 
 # In order to train with fewer images, this part will mix and select a portion (dataset_portion) of the entire training dataset.
 num_samples = len(val_dataset_full)
@@ -113,7 +128,7 @@ val_dataset = Subset(val_dataset_full, indices)
 
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-tot_batches = int(len(train_dataset)/batch_size)
+tot_batches = int(len(train_dataset) / batch_size)
 
 print(f"Tiles Dimension: {tile_dimension}x{tile_dimension}")
 
@@ -123,6 +138,8 @@ print(f'Validation dataset dimension: {len(val_dataset)}')
 print(f"{tot_batches} batches of {batch_size} images")
 
 if weightedBCE:
+
+    loss_name = 'WBCE'
     # First attempt. I need to change for the frequency of buildings and background pixels
     start = time.time()
     print('Computing the weights...')
@@ -135,16 +152,30 @@ if weightedBCE:
     neg_freq = tile_length * len(train_loader.dataset) - pos_freq
     weight = torch.tensor(neg_freq / pos_freq)
     print(f'Positive weight:\t{weight:.2f}')
-    criterion = nn.BCEWithLogitsLoss(pos_weight=weight)
+    bce_criterion = nn.BCEWithLogitsLoss(pos_weight=weight)
 else:
-    criterion = nn.BCEWithLogitsLoss()
+    loss_name = 'BCE'
+    bce_criterion = nn.BCEWithLogitsLoss()
+
+if diceLoss:
+    loss_name += 'plusDL'
+    dice_criterion = DiceLoss()
+else:
+    dice_criterion = None
 
 # model initialization, loss and optimizer
-model = UNet1(n_channels=3, n_classes=1).to(device)
+if architecture_name == 'unet':
+    model = UNet(n_channels=3, n_classes=1).to(device)
+elif architecture_name == 'unetL':
+    model = UNetL(n_channels=3, n_classes=1).to(device)
+elif architecture_name == 'unetLL':
+    model = UNetLL(n_channels=3, n_classes=1).to(device)
+
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+scheduler = optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.01, total_iters=20)
 
+model_name = f'{architecture_name}_{model_dataset}_{loss_name}_n{len(train_dataset)}_dim{tile_dimension}x{tile_dimension}_bs{batch_size}'
 
-model_name = f'unet_{model_dataset}_WBCE_lr{str(learning_rate).replace(".", "p")}_n{len(train_dataset)}_dim{tile_dimension}x{tile_dimension}_bs{batch_size}'
 os.makedirs(f'/home/antoniocorvino/Projects/BuildingsExtraction/runs/{model_name}', exist_ok=True)
 
 # Saving metrics
@@ -157,20 +188,23 @@ with open(csv_metrics, mode='w', newline='') as f:
                          "val_epoch_loss", "val_iou", "val_precision", "val_recall",
                          "time"])
 
-
 if torch.cuda.is_available():
     gpu_id = torch.cuda.current_device()
     print(f"\nGPU ID: {gpu_id}")
-    print(f"GPU Total Memory: {torch.cuda.get_device_properties(gpu_id).total_memory/1024**3:.2f} GB")
+    print(f"GPU Total Memory: {torch.cuda.get_device_properties(gpu_id).total_memory / 1024 ** 3:.2f} GB")
     torch.cuda.set_per_process_memory_fraction(memory_fraction, device=gpu_id)
 
+# %%
+best_loss = 1e4
 
 # Training loop
 for epoch in range(num_epochs):
-    print(f"EPOCH ---- {epoch+1}/{num_epochs}")
+    print(f"EPOCH ---- {epoch + 1}/{num_epochs}")
+    current_lr = optimizer.param_groups[0]['lr']
+    print(f'Current Learning Rate: {current_lr:.6f}')
     print("\nTraining is started...\n")
-    print(f"Memory Allocated: {torch.cuda.memory_allocated(gpu_id)/1024**3:.2f} GB")
-    print(f"Memory Reserved: {torch.cuda.memory_reserved(gpu_id)/1024**3:.2f} GB")
+    print(f"Memory Allocated: {torch.cuda.memory_allocated(gpu_id) / 1024 ** 3:.2f} GB")
+    print(f"Memory Reserved: {torch.cuda.memory_reserved(gpu_id) / 1024 ** 3:.2f} GB")
     start_time = time.time()
     model.train()
 
@@ -181,22 +215,24 @@ for epoch in range(num_epochs):
     recall_total = 0.0
 
     for images, masks in train_loader:
-        
+
         batch_number += 1
 
-        #print(f"\nBatch #{batch_number}")
+        # print(f"\nBatch #{batch_number}")
         images = images.to(device)
         masks = masks.to(device).float()  # This must be "float" for the loss function
 
-        #print(f"Memory allocated by the batch: {torch.cuda.memory_allocated(gpu_id)/1024**2:.2f} MB")
-        #print(f"Memory reserved by the batch: {torch.cuda.memory_reserved(gpu_id)/1024**2:.2f} MB")
-        #batch_memory = images.element_size() * images.nelement() + masks.element_size() * masks.nelement()
-        #print(f"Memory occupied by the batch (images and masks only): {batch_memory / 1024 ** 2:.2f} MB")
+        # print(f"Memory allocated by the batch: {torch.cuda.memory_allocated(gpu_id)/1024**2:.2f} MB")
+        # print(f"Memory reserved by the batch: {torch.cuda.memory_reserved(gpu_id)/1024**2:.2f} MB")
+        # batch_memory = images.element_size() * images.nelement() + masks.element_size() * masks.nelement()
+        # print(f"Memory occupied by the batch (images and masks only): {batch_memory / 1024 ** 2:.2f} MB")
 
         outputs = model(images)
 
-        loss = criterion(outputs, masks)
-
+        if dice_criterion:
+            loss = bce_criterion(outputs, masks)
+        else:
+            loss = bce_criterion(outputs, masks)
         with torch.no_grad():
             iou_batch = iou_score(outputs, masks)
             prec_batch = precision_score(outputs, masks)
@@ -213,7 +249,9 @@ for epoch in range(num_epochs):
         elapsed_time = time.time() - start_time
 
         if batch_number % int(0.1 * tot_batches) == 0:
-            print(f"\rProgress: {(100 * batch_number/tot_batches):.0f}% -- time: {int(elapsed_time//60):02d}:{int(elapsed_time%60):02d}", end="")
+            print(
+                f"\rProgress: {(100 * batch_number / tot_batches):.0f}% -- time: {int(elapsed_time // 60):02d}:{int(elapsed_time % 60):02d}",
+                end="")
 
         epoch_train_loss += loss.item()
     torch.cuda.empty_cache()
@@ -241,7 +279,7 @@ for epoch in range(num_epochs):
             val_masks = val_masks.to(device).float()
 
             val_outputs = model(val_images)
-            loss_val = criterion(val_outputs, val_masks)
+            loss_val = bce_criterion(val_outputs, val_masks)
             epoch_val_loss += loss_val.item()
 
             val_iou += iou_score(val_outputs, val_masks)
@@ -254,10 +292,18 @@ for epoch in range(num_epochs):
     val_prec = (val_prec / len(val_loader)).item()
     val_recall = (val_recall / len(val_loader)).item()
 
+    if epoch_val_loss < best_loss:
+        best_loss = epoch_val_loss
+        print(best_loss, epoch)
+        torch.save(model.state_dict(),
+                   f"/home/antoniocorvino/Projects/BuildingsExtraction/runs/{model_name}/best_model.pth")
+
     print(f"\nTime for Validation: {int(time.time() - starting_val):02d}s\n")
 
-    print(f'\nTRAINING\tLoss: {epoch_train_loss:.4f}\tIoU: {train_iou:.4f}\tPrecision: {train_prec:.4f}\tRecall: {train_recall:.4f}')
-    print(f"VALIDATION\tLoss: {epoch_val_loss:.4f}\tIoU: {val_iou:.4f}\tPrecision: {val_prec:.4f}\tRecall: {val_recall:.4f}\n\n")
+    print(
+        f'\nTRAINING\tLoss: {epoch_train_loss:.4f}\tIoU: {train_iou:.4f}\tPrecision: {train_prec:.4f}\tRecall: {train_recall:.4f}')
+    print(
+        f"VALIDATION\tLoss: {epoch_val_loss:.4f}\tIoU: {val_iou:.4f}\tPrecision: {val_prec:.4f}\tRecall: {val_recall:.4f}\n\n")
 
     # Save metrics to CSV
     with open(csv_metrics, mode='a', newline='') as f:
@@ -266,21 +312,12 @@ for epoch in range(num_epochs):
                              epoch_train_loss, train_iou, train_prec, train_recall,
                              epoch_val_loss, val_iou, val_prec, val_recall,
                              round(elapsed_time, 2)])
+    scheduler.step()
     # Checkpoint
-    if (epoch+1) % 10 == 0:
-        torch.save(model.state_dict(), f"/home/antoniocorvino/Projects/BuildingsExtraction/runs/{model_name}/checkpoint_{epoch+1}.pth")
-    if earlystop:
-        if epoch_val_loss < best_val_loss:
-            best_val_loss = epoch_val_loss
-            counter = 0
+    if (epoch + 1) % 10 == 0:
+        print(f"Checkpoint {epoch + 1}")
+        torch.save(model.state_dict(),
+                   f"/home/antoniocorvino/Projects/BuildingsExtraction/runs/{model_name}/checkpoint_{epoch + 1}.pth")
 
-        else:
-            early_stop_counter += 1
-            print(f"No improvement for {early_stop_counter} epoch(s).")
-
-        if early_stop_counter >= patience:
-            print("Early stopping triggered.")
-            torch.save(model.state_dict(), f"/home/antoniocorvino/Projects/BuildingsExtraction/runs/{model_name}/best_model.pth")
-
-            break
-print(f'Total time: {((time.time() - starting_time)/60):.2f} minutes')
+print(f'Total time: {((time.time() - starting_time) / 60):.2f} minutes')
+# %%
